1b8e54c1846b86d470dff577f182c523c0cf1909
goFrendiAsgard <gofrendiasgard@gmail.com>
2013-11-29 12:36:43 +0700

    tested and worked for MySQL

--- README.md
+++ README.md
@@ -103,13 +103,18 @@ How to use
 
     ```
 
+* Ensure your database & tables are already exists.
+
 * Make a python script just as in [test.py](test.py)
 
-    ```python
+    ```python        
+
         from csv2db import csv2db
 
         # connection string (used by sqlalchemy)
         connection_string = 'sqlite:///test.db'
+        # MySQL connection_string example. Beware of the charset
+        # connection_string = 'mysql://root:toor@localhost:3306/test?charset=utf8'
 
         # the csv file name. If your worksheet is on "xls" format, please convert them into csv first (i.e: in MS Excel you can use File | Save As)
         # the first line of the csv should be the header
@@ -118,10 +123,13 @@ How to use
         # more info about csv_param: http://docs.python.org/2/library/csv.html#csv-fmt-params
         csv_param = {
             'delimiter': ',',   # libre office usually use "," while microsoft office usually use "tab"
-            'quotechar': '"'
+            'quotechar': '"'    # both, libre office and microsoft office usually use '"'
         }
 
-        # define several preprocessing procedure (since human use different format from computer)
+        ########################################################################
+
+        # define several preprocessing callbacks. These callbacks will be used to preprocess every cell based on it's column
+
         def change_date_format(human_date):
             ''' change 08/31/2000 into 2000-08-31
             '''
@@ -134,15 +142,19 @@ How to use
             else:
                 computer_date = ''
             return computer_date
+
         def remove_dollar(value):
             ''' remove $, computer doesn't understand $
             '''
             return float(value.replace('$', ''))
 
+        # callback dictionary. The key is caption of the column, the value is the function used
         callback = {
             'Date' : change_date_format,
             'Price' : remove_dollar
         }
+
+        ########################################################################
 
         # specific preprocess function (in case of 2 different fields refer to the same csv column)
 
@@ -155,6 +167,8 @@ How to use
             ''' get "bottle" from "1 bottle"
             '''
             return ' '.join(value.split(' ')[1:])
+
+        ########################################################################
 
         # the table structure of your database and how they related to your csv file
         # WARNING: "unique" doesn't has any correlation with database unique constraint, unique is used as csv record identifier (since primary key does not exists in csv)
@@ -194,6 +208,10 @@ How to use
     ```
 
 * Run your python script, and your database should be filled automagically
+
+    ```
+        python test.py
+    ```
 
 Todo
 =====
--- test.py
+++ test.py
@@ -1,7 +1,53 @@
+# ignore this function, it is just used to generate test database
+def make_db(connection_string):
+    from sqlalchemy import Column, Integer, String, text, ForeignKey, Table, ForeignKeyConstraint, create_engine
+    from sqlalchemy.orm import relationship, backref, scoped_session, sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+
+    # create Base
+    Base = declarative_base()
+
+    class Transaction(Base):
+        __tablename__ = 'trans'
+        id = Column(Integer, primary_key=True)
+        code = Column(String(10))
+        date = Column(String(12))
+
+    class Item(Base):
+        __tablename__ = 'item'
+        id = Column(Integer, primary_key=True)
+        code = Column(String(10))
+        name = Column(String(20))
+        price = Column(Integer)
+        unit = Column(String(10))
+
+    class Transaction_Detail(Base):
+        __tablename__ = 'trans_detail'
+        id = Column(Integer, primary_key=True)
+        qty = Column(Integer)
+        id_transaction = Column(Integer, ForeignKey('trans.id'))
+        id_item = Column(Integer, ForeignKey('item.id'))
+
+    # create engine
+    engine = create_engine(connection_string, echo=True)
+
+    # create db session
+    db_session = scoped_session(sessionmaker(bind=engine))
+    Base.metadata.create_all(bind=engine)
+    db_session.commit()
+
+########################################################################
+
 from csv2db import csv2db
 
 # connection string (used by sqlalchemy)
 connection_string = 'sqlite:///test.db'
+# MySQL connection_string example. Beware of the charset
+# connection_string = 'mysql://root:toor@localhost:3306/test?charset=utf8'
+
+# create database if not exists (this is just for test case, you won't need this for real case)
+make_db(connection_string)
+
 
 # the csv file name. If your worksheet is on "xls" format, please convert them into csv first (i.e: in MS Excel you can use File | Save As)
 # the first line of the csv should be the header
@@ -10,10 +56,13 @@ file_name = 'test.csv'
 # more info about csv_param: http://docs.python.org/2/library/csv.html#csv-fmt-params
 csv_param = {
     'delimiter': ',',   # libre office usually use "," while microsoft office usually use "tab"
-    'quotechar': '"'
+    'quotechar': '"'    # both, libre office and microsoft office usually use '"'
 }
 
-# define several preprocessing procedure (since human use different format from computer)
+########################################################################
+
+# define several preprocessing callbacks. These callbacks will be used to preprocess every cell based on it's column
+
 def change_date_format(human_date):
     ''' change 08/31/2000 into 2000-08-31
     '''
@@ -26,15 +75,19 @@ def change_date_format(human_date):
     else:
         computer_date = ''
     return computer_date
+
 def remove_dollar(value):
     ''' remove $, computer doesn't understand $
     '''
     return float(value.replace('$', ''))
 
+# callback dictionary. The key is caption of the column, the value is the function used
 callback = {
     'Date' : change_date_format,
     'Price' : remove_dollar
 }
+
+########################################################################
 
 # specific preprocess function (in case of 2 different fields refer to the same csv column)
 
@@ -47,6 +100,8 @@ def filter_unit(value):
     ''' get "bottle" from "1 bottle"
     '''
     return ' '.join(value.split(' ')[1:])
+
+########################################################################
 
 # the table structure of your database and how they related to your csv file
 # WARNING: "unique" doesn't has any correlation with database unique constraint, unique is used as csv record identifier (since primary key does not exists in csv)
